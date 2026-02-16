87d76d348271f82cdeb66a90071c9eb9a904142f
Lolita0000 <anarmachadolu@gmail.com>
2026-02-16 13:58:02 -0300

    global style

--- src/componentes/OpcoesHeader/index.jsx
+++ src/componentes/OpcoesHeader/index.jsx
@@ -1,14 +1,30 @@
-import './estilo.css'
+import styled from 'styled-components';
+
+const Opcao = styled.li`
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  height: 100%;
+  padding: 0 5px;
+  cursor: pointer;
+  min-width: 120px;
+`
+
+const Opcoes = styled.ul`
+  display: flex;
+`
 
 const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
 
 function OpcoesHeader() {
     return (
-        <ul className='Opcoes'>
+        <Opcoes>
             {textoOpcoes.map((texto) => (
-                <li className='opcao'><p>{texto}</p></li>
+                <Opcao><p>{texto}</p></Opcao>
             ))}
-        </ul>
+        </Opcoes>
     )
 }
 
